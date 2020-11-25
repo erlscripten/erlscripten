a2aacfbdd0da5b7509b57bfcde8a96b510d4c6ed
Grzegorz Uriasz <gorbak25@gmail.com>
2020-11-25 14:51:43 +0100

    Lists now properly coroutinize

--- coroutinize.sh
+++ coroutinize.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cat $1 | sed -E "s/(erlps__.*__[0-9]+ = )function/\1function\*/g" | sed -E "s/(erlps__.*__[0-9]+\()/yield\* \1/g" | sed -E "s/function (.tco_loop)/function\* \1/g" | sed -E "s/= (.tco_loop\()/= yield\* \1/g" > /tmp/a
+cat $1 | sed -E "s/(erlps__.*__[0-9]+ = )function/\1function\*/g" | sed -E "s/(erlps__.*__[0-9]+\()/yield\* \1/g" | sed -E "s/function (.tco_loop)/function\* \1/g" | sed -E "s/= (.tco_loop\()/= yield\* \1/g" | sed -E "s/\w+\(true\)/yield* &/g" | sed -E "s/\(function \(\) \{/\yield* (function\* \(\) \{/g" | sed -E "s/= function \(/= function\* \(/g" > /tmp/a
 cp /tmp/a $1
 
