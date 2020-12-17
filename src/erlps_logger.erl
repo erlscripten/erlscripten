7ec511961ec7e42e356df3677b900bf4c8a18eeb
radrow <radrowicki@gmail.com>
2020-12-17 16:17:23 +0100

    Colors <3

--- src/erlps_logger.erl
+++ src/erlps_logger.erl
@@ -5,18 +5,18 @@
 -export([die/2, debug/1, debug/2, info/1, info/2]).
 
 die(File, Reason) ->
-    io:format(user, "Erlscripten encountered a fatal error while compiling ~s\n~s\n", [File, iolist_to_binary(Reason)]),
+    io:format(user, "\e[1;31m[ERROR]\e[1;37m While compiling\e[0m ~s\e[1;37m:\e[0m\n~s\n", [File, iolist_to_binary(Reason)]),
     error(die).
 
 debug(Format) ->
-    info(Format, []).
+    debug(Format, []).
 debug(Format, Args) ->
-    print_with_tag("DEBUG", Format, Args).
+    print_with_tag("\e[1;33m[DEBUG]\e[0m", Format, Args).
 
 info(Format) ->
     info(Format, []).
 info(Format, Args) ->
-    print_with_tag("INFO", Format, Args).
+    print_with_tag("\e[1;36m[INFO]\e[0m", Format, Args).
 
 print_with_tag(Tag, Format, Args) ->
-    io:format(user, "[~s] ~s\n", [Tag, iolist_to_binary(io_lib:format(Format, Args))]).
+    io:format(user, "~s ~s\n", [Tag, iolist_to_binary(io_lib:format(Format, Args))]).
